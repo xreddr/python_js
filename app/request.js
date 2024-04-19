05e1d51cf006618ed8e652917b93a7a0491ea406
Ryan Henion <65464043+xreddr@users.noreply.github.com>
2024-04-18 20:39:56 -0400

    Update request.js

--- app/request.js
+++ app/request.js
@@ -14,3 +14,23 @@ fetch('http://127.0.0.1:5000/0/omega')
         console.log('GET response:');
         console.log(text);
     });
+
+// Copied from pcx repos js_client
+
+// Request data
+var data = {
+    service : "service1",
+    username : "jelly",
+    password : "password"
+}
+
+// POST requests
+fetch("http://34.42.125.145:8080/api/login", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+.then(response => response.json())
+.then(json => console.log(json));
